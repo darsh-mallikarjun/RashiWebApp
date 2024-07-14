1ad7dd5b948498e4c2aaaeb786ef4d437e84d67e
darshanmallikarjun194@gmail.com <darshanamllikarjun194@gamil.com>
2024-07-14 21:20:47 +0530

    target

---
+++ target/classes/static/js/confirm.js
@@ -0,0 +1,11 @@
+ let popup = document.getElementById("popup");
+    
+    function OpenPopup(){
+    	popup.classList.add("open-popup");
+    }
+    
+
+    function ClosePopup(){
+    	popup.classList.remove("open-popup");
+    }
+    
